9f5f8ae067e0ba3da95a41d4e609b8781293acae
arimaulanahardan <arimaulanahardan@gmail.com>
2024-08-04 20:36:08 +0700

    intro

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-400  ">Hello world!</h1>
+    </>
+  );
+};
+
+export default App;
