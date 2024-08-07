ec1b1fe6414f049836fe61f569589f0b8cde1b8b
arimaulanahardan <arimaulanahardan@gmail.com>
2024-08-07 16:54:24 +0700

    add : smart contract configuration

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -1,10 +1,19 @@
 const Loader = () => {
-    return (
-      <>
-        <h1>Loader</h1>
-      </>
-    );
-  };
-  
-  export default Loader;
-  
+  return (
+    <>
+      <button
+        type="button"
+        className="pointer-events-none inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 disabled:opacity-70 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+        disabled
+      >
+        <div
+          className="inline-block h-4 w-4 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
+          role="status"
+        ></div>
+        <span>Loading...</span>
+      </button>
+    </>
+  );
+};
+
+export default Loader;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,36 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
+  mode : 'jit',
   theme: {
-    extend: {},
+    fontFamily: {
+      display : ['Open-Sans', 'sans-serif'],
+      body: ['Open-Sans', 'sans-serif'],
+    },
+    screens: {
+      mf: "990px",
+    },
+    keyframes: {
+      "slide-in": {
+        "0%": {
+          "-webkit-transform": "translateX(120%)",
+          transform: "translateX(120%)",
+        },
+        "100%": {
+          "-webkit-transform": "translateX(0%)",
+          transform: "translateX(0%)",
+        },
+      },
+    },
+    animation: {
+      "slide-in": "slide-in 0.5s ease-out",
+    },
+    variants: {
+      extend: {},
+    },
   },
-  plugins: [],
+  plugins: [
+    // require("@tailwindcss/forms")
+  ],
 }
